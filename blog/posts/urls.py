d12773679b15ac6d7c7c5417d4a1de7b3ed28395
ciya <ciya>
2017-09-19 16:20:02 +0200

    Added href to the posts and link them with an URL pattern. This was the latest tutorial I will work in this repository.

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -4,4 +4,8 @@ from posts.models import Post
 
 urlpatterns = [ url(r'^$', ListView.as_view(
                                     queryset=Post.objects.all().order_by("-date")[:20],
-                                    template_name="posts/posts.html")),]
+                                    template_name="posts/posts.html")),
+                url(r'^(?P<pk>\d+)$', DetailView.as_view( #pk means primary key, \d means digit, + means "one or more", \d+ meeans one or more digits.
+                                    model = Post,
+                                    template_name="posts/post.html")),
+                ]
